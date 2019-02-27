9cd491084e9dba8f6db0a31a05c673aa7d9ab11e
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 07:33:17 +0900

    #1-1 make a component

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,3 @@
-#MOVIEリストを作ります！
-
-yarn install global create-react-app
-
-create-react-app project name
-
-yarn start
-
-컴포넌트 만드는것 (ex: Header.js and Header.css)
-
 import React, { Component } from 'react';
 import './Header.css';
 

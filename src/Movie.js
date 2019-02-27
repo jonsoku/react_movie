4e45c07367603a502d5514dea9f82374d041247c
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 07:37:45 +0900

    하나의 컴포넌트에 미니컴포넌트 넣기

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,19 @@ import './Movie.css';
 class Movie extends Component {
     render() {
         return (
-            <h1>movie component</h1>
+            <div>
+                <h1>movie component</h1>
+                <MoviePoster/>
+            </div>
+
+        );
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return (
+            <img src="https://images.kbench.com/kbench/article/2013_08/k122549p1n1.jpg" alt=""/>
         );
     }
 }

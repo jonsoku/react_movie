d7463ec1dcd001e5b01c683f3fd53932264c11b2
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 10:16:00 +0900

    Promises

--- src/App.js
+++ src/App.js
@@ -36,34 +36,10 @@ class App extends Component {
     }
 
     componentDidMount(){
-        setTimeout(() => {
-            this.setState({
-                movies: [
-                    {
-
-                        title:"매트릭스",
-                        poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-                    },
-                    {
-                        title:"올드보이",
-                        poster: "http://artinsight.co.kr/n_news/peg/1508/thumb/a4cb4757610239a28cf0cba125b24a69_vpZESkDO68sOb6AbZkW9hFd.jpg"
-                    },
-                    {
-                        title:"secret",
-                        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Secret-Bunengshuodemimi2.jpg/220px-Secret-Bunengshuodemimi2.jpg",
-                    },
-                    {
-                        title:"스타워즈",
-                        poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg"
-                    },
-                    {
-                        title:"시간을 달리는 소녀",
-                        poster:"https://t1.daumcdn.net/movie/7da2ccc618ca4dbb971379944a939fbf1541747671440"
-
-                    }
-                ]
-            })
-        }, 2000)
+        fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err=>console.log(err))
     }
 
     _renderMovies = () => {

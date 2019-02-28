6391a1c35a4ff981650c3ed143c6a183c973f896
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 10:51:33 +0900

    업데이트 컴포넌트

--- src/Movie.js
+++ src/Movie.js
@@ -38,29 +38,53 @@ import './Movie.css';
 //         );
 //     }
 // }
-function Movie({title,poster}){
+function Movie({title,poster, genres, synopsis}){
     return (
-        <div>
-            <h1>{title}</h1>
-            <MoviePoster poster={poster}/>
+        <div className="Movie">
+            <div className="Movie__Colums">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Colums">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre,index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     );
 }
 
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt=""/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+    );
+}
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genres">{genre}</span>
     );
 }
 
+
 Movie.prototype = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres:PropTypes.string.isRequired,
+    synopsis:PropTypes.string.isRequired,
+
 }
 
 MoviePoster.propTypes ={
     poster: PropTypes.string.isRequired
 }
+
+MovieGenre.propTypes ={
+    genre: PropTypes.string.isRequired
+}
+
 
 export default Movie;
